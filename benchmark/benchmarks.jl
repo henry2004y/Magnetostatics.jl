40d9541ee1c01409070d393d5808b01b9c4299e0
Hongyang Zhou <hyzhou@umich.edu>
2026-02-15 21:48:24 -0500

    Redesign test case

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -10,18 +10,20 @@ SUITE["solvers"] = BenchmarkGroup()
 SUITE["solvers"]["biot_savart"] = BenchmarkGroup()
 let
     r = SVector(0.5, 0.5, 0.0)
-    # Wire from (0,0,-1) to (0,0,1) with current 1.0
-    wire = Wire([SVector(0.0, 0.0, -1.0), SVector(0.0, 0.0, 1.0)], 1.0)
+    loop = CurrentLoop(1.0, 1.0, SVector(0.0, 0.0, 0.0), SVector(0.0, 0.0, 1.0))
+    # creates a 100-segment wire
+    wire = discretize_loop(loop, 100)
     solver = BiotSavart()
 
-    SUITE["solvers"]["biot_savart"]["simple_wire"] = @benchmarkable solve($solver, $wire, $r)
+    SUITE["solvers"]["biot_savart"]["discretized_loop"] = @benchmarkable solve($solver, $wire, $r)
 end
 
 # Analytical
 SUITE["solvers"]["analytical"] = BenchmarkGroup()
 let
     r = SVector(0.5, 0.0, 0.1)
-    loop = CurrentLoop(3.0, 1.0, SVector(0.0, 0.0, 0.0), SVector(0.0, 0.0, 1.0)) # radius=3, current=1
+    # radius=3, current=1
+    loop = CurrentLoop(3.0, 1.0, SVector(0.0, 0.0, 0.0), SVector(0.0, 0.0, 1.0))
 
     SUITE["solvers"]["analytical"]["current_loop"] = @benchmarkable getB_loop($r, $loop)
 end
